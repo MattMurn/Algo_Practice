3acdb27472e43ae827e01df5f3ec2b5736e7be7d
Matthew Murnighan <mattmurn@gmail.com>
2018-09-08 08:30:19 -0500

    updated scope

--- Scope/app.js
+++ Scope/app.js
@@ -15,10 +15,55 @@ const screamWarrior = () => {
         }
     }
 }
-const newWarrior = screamWarrior();
+// const newWarrior = screamWarrior();
 
-newWarrior.shootWarrior();
-screamWarrior();
-console.log(warrior_3)
+// newWarrior.shootWarrior();
+// screamWarrior();
+// console.log(warrior_3)
 
 // console.log(warrior, warrior_2)
+
+function chance(name, inst) {
+    this.name = name;
+    this.inst = inst;
+    this.printChance = function() {
+        console.log(`this is a constructor and its properities -- name: ${this.name}, inst: ${this.inst}`);
+    }
+}
+
+let jerry = new chance('jerry', 'Northwestern');
+
+// jerry.printChance();
+
+class Change {
+    constructor(name="Barry", inst="San Fransisco") {
+        this.name = name;
+        this.inst = inst;
+        this.price = 899
+    }
+    
+    printChance() {
+        console.log(`this is a constructor and its properities -- name: ${this.name}, inst: ${this.inst}`);
+    }
+    cancelDaSeason() {
+        console.log(`the cost of this season to get through to the playoff is ${this.price} on average`)
+    }
+}
+
+class Bonds extends Change {
+    constructor(name, inst, ba){
+        super(name, inst);
+        this.ba =ba;
+    }
+    printBa() {
+        console.log(`${this.name}'s batting average is ${this.ba}`)
+    }
+}
+let barry = new Change('Barry', 'Illinois');
+
+barry.printChance();
+barry.cancelDaSeason();
+
+let gary = new Bonds('Barry', 'San Fransisco', .321)
+
+gary.printBa()
