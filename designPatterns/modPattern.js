bdbef0be362b4a4219ff130c761a0f366140869c
Matthew Murnighan <mattmurn@gmail.com>
2018-10-05 10:12:15 -0500

    dps

--- designPatterns/modPattern.js
+++ designPatterns/modPattern.js
@@ -26,7 +26,7 @@ var basketMod = (()=>{
     }
 
 })();
-console.log(basketMod.getQuant())
+// console.log(basketMod.getQuant())
 basketMod.addItem({
     item: 'cheese',
     price: 2.99
@@ -35,7 +35,67 @@ basketMod.addItem({
     item: 'bread', 
     price: 1.99
 })
-console.log(basketMod.getQuant())
+// console.log(basketMod.getQuant())
 basketMod.clear();
-console.log(basketMod.getQuant())
-console.log(basketMod.getTotal())
+// console.log(basketMod.getQuant())
+// console.log(basketMod.getTotal())
+
+
+
+let revealingModule = (()=> {
+    let priVar = 'Ben Hogan'
+    let pubVar = 'Hello World'
+
+    privFunction = () => {
+        console.log(`Name: ${priVar}`)
+    }
+    pubSetName = str => {
+        priVar = str;
+    }
+    pubGetName = () => {
+        privFunction();
+    }
+
+    return {
+        setName: pubSetName,
+        greeting: pubVar,
+        getName: pubGetName
+    }
+})();
+
+
+revealingModule.setName('Jerome');
+
+revealingModule.getName();
+
+console.log(revealingModule.greeting);
+
+
+firstSingle = (()=> {
+    let instance;
+    init = () => {
+        privMethod = () => {
+            console.log('I am private');
+        } 
+        let privVar = 'also a private variable';
+        let priRandomNumber = Math.floor(10);
+
+        return {
+            pubMethod: () => {
+                console.log('this is a public method')
+            },
+            pubProp: 'also public',
+            getRandom: () => {
+                return priRandomNumber;
+            }
+        }
+    };
+    return {
+        getInstance: () => {
+            if(!instance) {
+                instance = init();
+            }
+            return instance;
+        }
+    }
+})
